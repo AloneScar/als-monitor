713a3750f64ba475034a38ddc032a915bbbf2db5
alonescar <3245632373@qq.com>
2022-12-03 16:12:37 +0800

    2/3

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 import os
 import json
 import sqlite3
+import snowflake.client
+
 
 settings_file_path = './settings.json'
 database_folder_path = './database'
+
+def get_snowflake_uuid() -> int:
+    return snowflake.client.get_guid()
 
 def load_settings_file() -> dict:
     if not os.path.exists(settings_file_path):
@@ -14,16 +19,18 @@ def load_settings_file() -> dict:
     with open(mode="r", file=settings_file_path) as f:
         return json.load(f)
 
-def init_database(db_path):
+def init_database(db_name):
     if not os.path.exists(database_folder_path):
         os.mkdir(database_folder_path)
 
-    conn = sqlite3.connect(db_path)
+    conn = sqlite3.connect(os.path.join(database_folder_path, db_name + ".db"))
     cur = conn.cursor()
 
     sql = '''
-        create table if not exists monitor (
-            id int primary
+        create table if not exists files_info (
+            id integer primary key autoincrement,
+            path text,
+            timestamp text
         )
     '''
 
@@ -32,7 +39,54 @@ def init_database(db_path):
 
     return (conn, cur)
 
+def get_files_info(folder_name):
+    files_info = []
+    for curDir, _,  files in os.walk(folder_name):
+        for file in files:
+            print('aaa')
+            file_path = os.path.join(curDir, file)
+            files_info.append((
+                file_path,
+                os.stat(file_path).st_mtime,
+            ))
+    return files_info
+
+def comparison_files_info(current_files_info, cur):
+    updated_files_info = []
+
+    cur.execute("select path, timestamp from files_info")
+    pervious_files_info = cur.fetchall()
+
+    pervious_paths = set([path[0] for path in pervious_files_info])
+    current_paths = set([path[0] for path in current_files_info])
+
+    intersection_paths = pervious_paths & current_paths
+
+    added_path = current_paths - intersection_paths
+    deleted_path = pervious_paths - intersection_paths
+
+    pervious_timestamps = [timestamp[1] for timestamp in pervious_files_info]
+
+    for file_info in current_files_info:
+        if file_info[0] in current_paths or file_info[0] in pervious_paths:
+            continue
+        pervious_index = pervious_paths.index(file_info[0])
+        if file_info[0] != pervious_timestamps[pervious_index]:
+            updated_files_info.append(pervious_files_info[pervious_index])
+    
+    return (added_path, deleted_path, updated_files_info)
+
+def updated_files_info(added_files_info, deleted_path, cur, conn):
+    pass
+
+def main():
+    for folder_path in load_settings_file()['monitor_folders']:
+        # _, folder_name = os.path.split(folder_path)
+        conn, cur = init_database(folder_path.replace('\\', "-").replace('/', "-").replace(':', '-'))
+        current_files_info = get_files_info(folder_path)
+        # added_path, deleted_path, updated_files_info = comparison_files_info(current_files_info, cur)
+        print(comparison_files_info(current_files_info, cur))
+
+
 if __name__ == "__main__":
-    load_settings_file()
-    for folder in load_settings_file()['monitor']:
-        conn, cur = init_database(folder)
+    main()
